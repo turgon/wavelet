0969c41979880e709ba4d9c2b4c19d676d19d3c4
Nick S <turgon@gmail.com>
2016-01-09 06:44:29 +0000

    messy, but functional RRR Rank

---
+++ rrr/rrr.go
@@ -0,0 +1,100 @@
+package rrr
+
+import (
+	"github.com/turgon/wavelet/bitfield"
+)
+
+// for now let's go with block size of 8 bits
+// and superblock size of 4 blocks.
+// so a superblock is 32 bits.
+
+const blockSize = 8      // bits
+const superblockSize = 4 // blocks
+
+type RRR struct {
+	bf          *bitfield.BitField
+	blocks      uint
+	superblocks uint
+
+	// this places a max length of 4294967296 symbols on the doc.
+	superRanks []uint32
+}
+
+func NewRRR(bf *bitfield.BitField) RRR {
+	// how many blocks are in this bitfield?
+	blocks := uint(bf.Len() / blockSize)
+	if bf.Len()%blockSize > 0 {
+		blocks++
+	}
+	superblocks := uint(blocks / superblockSize)
+	if blocks%superblockSize > 0 {
+		superblocks++
+	}
+
+	r := RRR{
+		bf,
+		blocks,
+		superblocks,
+		make([]uint32, superblocks, superblocks),
+	}
+
+	var tot uint32
+	for i := uint(0); i < superblocks-1; i++ {
+		for j := uint(0); j < superblockSize; j++ {
+			loc := i * superblockSize + j
+			if loc < uint(len(bf.Data)) {
+				tot += popcountByte(bf.Data[loc])
+			}
+		}
+		r.superRanks[i+1] = tot
+	}
+
+	return r
+}
+
+func (r *RRR) Rank(x uint) (tot uint32) {
+	// I need to return the sum of ranks for all prior superblocks
+	// plus the sum of the ranks for all blocks within this
+	// superblock prior to x,
+	// plus the rank of this block up to x.
+
+	if x >= r.bf.Len() {
+		x = r.bf.Len() - 1
+	}
+
+	block := x / blockSize
+
+	superblock := block / superblockSize
+
+	tot = r.superRanks[superblock]
+
+	for i := superblock * superblockSize; i < block; i++ {
+		tot += popcountByte(r.bf.Data[i])
+	}
+
+	blockOffset := x % blockSize
+
+	if block != uint(len(r.bf.Data)) {
+		tot += popcountByte((r.bf.Data[block] >> (8 - blockOffset)))
+	}
+
+	return tot
+}
+
+func popcountByte(z byte) uint32 {
+
+	b0 := z & 85
+	b1 := (z >> 1) & 85
+
+	c := b0 + b1
+
+	d0 := c & 51
+	d2 := (c >> 2) & 51
+
+	e := d0 + d2
+
+	f0 := e & 15
+	f4 := (e >> 4) & 15
+
+	return uint32(f0 + f4)
+}
---
+++ rrr/rrr_test.go
@@ -0,0 +1,84 @@
+package rrr
+
+import (
+	"github.com/turgon/wavelet/bitfield"
+	"testing"
+	"math/rand"
+)
+
+func TestNewRRR(t *testing.T) {
+	bf := bitfield.NewBitField(41)
+
+	bf.Set(2)
+	bf.Set(10)
+	bf.Set(35)
+
+	r := NewRRR(&bf)
+
+	for i := uint(0); i < r.superblocks; i++ {
+		for j := uint(0); j < superblockSize; j++ {
+			// t.Errorf("%v %v %v\n", i * superblockSize, j, i * superblockSize + j)
+		}
+	}
+
+	// t.Errorf("%v\n", r)
+}
+
+func TestRank(t *testing.T) {
+	bf := bitfield.NewBitField(8 * 5)
+	for i := uint(0); i < 8*5; i++ {
+		bf.Set(i)
+	}
+	r := NewRRR(&bf)
+	for i := uint(0); i < 8*5; i++ {
+		if r.Rank(i) != uint32(i) {
+			t.Fatalf("%v\n", r.Rank(i))
+		}
+	}
+}
+
+func BenchmarkRank(b *testing.B) {
+
+	rs := make([]RRR, 100, 100)
+
+	for i := 0; i < 100; i++ {
+		var bf bitfield.BitField
+
+		bits := 512 + rand.Intn(512)
+
+		bf = bitfield.NewBitField(uint(bits))
+		for j := rand.Intn(bits/2); j >= 0; j-- {
+			bf.Set(uint(rand.Intn(bits)))
+		}
+
+		rs[i] = NewRRR(&bf)
+	}
+
+	for i := 0; i < b.N; i++ {
+		var r RRR
+		r = rs[i % 100]
+
+		r.Rank(r.bf.Len()-1)
+	}
+}
+
+func TestPopcountBye(t *testing.T) {
+	var x byte
+
+	for i := uint16(0); i < 256; i++ {
+		x = byte(i)
+		if popcountByte(x) != naivePopCount(x) {
+			t.Fatalf("popcountByte did not match naivePopCount for x=%v\n", x)
+		}
+	}
+}
+
+func naivePopCount(x byte) (c uint32) {
+	for i := 0; i < 8; i++ {
+		if x&1 > 0 {
+			c++
+		}
+		x >>= 1
+	}
+	return c
+}
