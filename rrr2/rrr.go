0364f91ed06ee41273fa586bb1e1027959d366db
Nick S <turgon@gmail.com>
2016-01-24 19:34:03 +0000

    experimenting with pure bitfield representation

---
+++ rrr2/rrr.go
@@ -0,0 +1,134 @@
+package rrr2
+
+import (
+	"math"
+	"math/big"
+
+	"github.com/turgon/wavelet/bitfield"
+)
+
+type RRRField struct {
+	bitfield.BitField
+
+	blockSize uint
+	superSize uint
+
+	superRanks uint64
+
+	classBits uint8
+	offsetBits uint8
+
+	stepBits uint8
+
+	lastBlockLen uint
+
+	global map[uint64][]bitfield.BitField
+}
+
+// Class fetches the class of the block at the given position.
+func (r RRRField) Class(block uint) uint64 {
+	start := uint(r.stepBits) * block
+	end := start + uint(r.classBits)
+
+	bf := r.Sub(start, end)
+	return bf.Uint64(uint(r.classBits))
+}
+
+// Offset fetches the offset of the block at the given position.
+func (r RRRField) Offset(block uint) uint64 {
+	start := uint(r.stepBits) * block + uint(r.classBits)
+	end := start + uint(r.offsetBits)
+
+	bf := r.Sub(start, end)
+
+	return bf.Uint64(uint(r.offsetBits))
+}
+
+func (r RRRField) Block(block uint) bitfield.BitField {
+	b := r.global[r.Class(block)][r.Offset(block)]
+
+	if block == r.Len() / uint(r.stepBits) - 1 {
+		b = b.Resize(r.lastBlockLen)
+	}
+
+	return b
+}
+
+// NewRRRField returns an RRRField generated from the bitfield
+// given as bf. The bitfield will be blocked in blockSize bits,
+// and each superSize blocks will be summarized.
+func NewRRRField(bf *bitfield.BitField, blockSize uint, superSize uint) RRRField {
+	var r RRRField
+
+	r.blockSize = blockSize
+	r.superSize = superSize
+
+	r.classBits = needBits(uint64(blockSize + 1))
+	r.offsetBits = bitsForLargest(uint8(blockSize))
+
+	r.stepBits = r.classBits + r.offsetBits
+
+	r.global = make(map[uint64][]bitfield.BitField, 0)
+
+	offMap := make(map[uint64]map[uint64]uint64)
+	offMax := make(map[uint64]uint64)
+
+	for i := uint(0); i < bf.Len(); i += blockSize {
+		sub := bf.Sub(i, i + blockSize)
+		subpc := sub.Popcount()
+
+		r.lastBlockLen = sub.Len()
+
+		// create a bitfield consisting of the number of
+		// bits necessary to describe any class, and set
+		// it to this class.
+		cf := bitfield.NewBitFieldFromUint64(uint(r.classBits), subpc)
+		r.BitField = r.CopyBits(cf, uint(r.stepBits) * (i / blockSize), cf.Len())
+
+		if _, ok := offMap[subpc]; !ok {
+			offMap[subpc] = make(map[uint64]uint64)
+			r.global[subpc] = make([]bitfield.BitField, 0)
+		}
+
+		subval := sub.Uint64(sub.Len())
+
+		var offset uint64
+
+		if os, ok := offMap[subpc][subval]; !ok {
+			offMap[subpc][subval] = offMax[subpc]
+			offset = offMax[subpc]
+			offMax[subpc]++
+		} else {
+			offset = os
+		}
+
+		r.global[subpc] = append(r.global[subpc], sub)
+
+		of := bitfield.NewBitFieldFromUint64(uint(r.offsetBits), offset)
+		r.BitField = r.CopyBits(of, uint(r.stepBits) * (i / blockSize) + uint(r.classBits), of.Len())
+	}
+
+	return r
+}
+
+// needBits takes a number x and returns the number of bits needed
+// to store the range [0, x).
+func needBits(x uint64) uint8 {
+	return uint8(math.Ceil(math.Log2(float64(x))))
+}
+
+// bitsForLargest takes a number n of RRR classes, figures out the
+// size of the largest class, and returns the number of bits needed
+// to enumerate that class. For example, given 8 classes, the largest
+// class has 8/2 = 4 bits set with (8 choose 4) = 70 permutations.
+// Thus, bitsForLargest(8) = ceiling(log2(70)) = 7 bits.
+func bitsForLargest(n uint8) uint8 {
+
+	if n > 64 {
+		panic("bitForLargest: n can't exceed 64")
+	}
+
+	i := new(big.Int)
+	i.Binomial(int64(n), int64(n/2))
+	return needBits(uint64(i.Int64()))
+}
---
+++ rrr2/rrr_test.go
@@ -0,0 +1,117 @@
+package rrr2
+
+import (
+	"testing"
+	"github.com/turgon/wavelet/bitfield"
+)
+
+func TestNewRRRField(t *testing.T) {
+        bf := bitfield.NewBitField(24)
+
+        for i := uint(0); i < 4; i++ {
+                bf.Set(i)
+        }
+        for i := uint(8); i < 24; i+=2 {
+                bf.Set(i)
+        }
+
+	r := NewRRRField(&bf, 8, 10)
+	if r.Class(0) != 4 {
+		t.Errorf("NewRRRField has wrong class: %v", r.Class(0))
+	}
+	if r.Class(1) != 4 {
+		t.Errorf("NewRRRField has wrong class: %v", r.Class(1))
+	}
+
+	if r.Offset(0) != 0 {
+		t.Errorf("NewRRRField has wrong offset: %v", r.Offset(0))
+	}
+	if r.Offset(1) != 1 {
+		t.Errorf("NewRRRField has wrong offset: %v", r.Offset(1))
+	}
+}
+
+func TestNeedBits(t *testing.T) {
+	var j uint8
+	for i := uint64(2); i < 128; i *= 2 {
+		j++
+		if j != needBits(i) {
+			t.Errorf("needBits returned %v for %v; should have been %v\n", needBits(i), i, j)
+		}
+	}
+}
+
+func TestBitsForLargest(t *testing.T) {
+	var b uint8
+
+	b = bitsForLargest(8)
+	if b != 7 {
+		t.Errorf("bitsForLargest for 8 was %v but should be 7", b)
+	}
+
+	b = bitsForLargest(64)
+	if b != 61 {
+		t.Errorf("bitsForLargest for 64 was %v but should be 61", b)
+	}
+}
+
+func TestRRRFieldClass(t *testing.T) {
+        bf := bitfield.NewBitField(30)
+
+	bf.Set(5)
+
+	bf.Set(10)
+	bf.Set(11)
+
+	bf.Set(15)
+	bf.Set(16)
+	bf.Set(17)
+
+	bf.Set(20)
+	bf.Set(21)
+	bf.Set(22)
+	bf.Set(23)
+
+	bf.Set(25)
+	bf.Set(26)
+	bf.Set(27)
+	bf.Set(28)
+	bf.Set(29)
+
+	r := NewRRRField(&bf, 5, 10)
+
+	for i := uint(0); i <= 5; i++ {
+		if r.Class(i) != uint64(i) {
+			t.Errorf("Class returned wrong value: %v", r.Class(0))
+		}
+	}
+}
+
+func TestRRRFieldBlock(t *testing.T) {
+	bf := bitfield.NewBitField(37)
+
+	pl := []uint{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
+
+	for _, p := range pl {
+		bf.Set(p)
+	}
+
+	r := NewRRRField(&bf, 5, 10)
+
+	bf2 := bitfield.NewBitField(37)
+
+	for i := uint(0); i < r.Len() / uint(r.stepBits); i++ {
+		b := r.Block(i)
+		bf2 = bf2.CopyBits(b, i * r.blockSize, b.Len())
+	}
+
+	if bf.Len() != bf2.Len() {
+		t.Fatalf("Rebuilt bitfield lengths don't match")
+	}
+
+	for i := uint(0); i < bf.Len(); i++ {
+		if bf.Test(i) != bf2.Test(i) {
+			t.Errorf("Rebuilt bitfield doesn't match at position %v", i)
+		}
+	}
+}
